2b9ad2819f442f5988160608e422653ad1c9fdb5
Aarav Gupta <agupta@thegraysonschool.org>
2025-11-05 15:13:26 -0500

    added bubbleSort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,4 +1,17 @@
-def bubbleSort(n):
-    print("hello world")
+def bubbleSort(curList):
+    n = len(curList)
+    while True:
+        k = True
+        for i in range(0, n-1):
+            if curList[i] > curList[i+1]:
+                curList[i], curList[i+1] = curList[i+1], curList[i]
+                k = False
+        if k:
+            break
 
-bubbleSort(3)
+
+    return curList
+
+
+
+print(bubbleSort([6, 5, 4, 3]))
