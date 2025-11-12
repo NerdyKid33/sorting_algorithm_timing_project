e13397dd8d90bd3c1cd5c9ca6d5443960919ba5a
Aarav Gupta <agupta@thegraysonschool.org>
2025-11-12 15:13:27 -0500

    started working with plot_fib

--- main.py
+++ main.py
@@ -3,4 +3,16 @@ from sortingAlgorithms import *
 from timing import time_function 
 from tableInText import *
 
+def plot_fib(n):
+    'Generate a plot showing how long it took to compute the ith fibonnaci number.'
+    import matplotlib.pyplot as plt
 
+    times = []
+    for i in range(n):
+        t, _ =  insertionSort(reversed(i))
+        times.append(t)
+
+    plt.plot(times)
+    plt.show()
+
+plot_fib(1000)
